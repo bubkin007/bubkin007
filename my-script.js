c4dac53153b518dd39d29e0460cfbab4d2e988a6
Aleksandr Babkin <bubkin-007@ya.ru>
2024-07-02 08:58:40 +0300

    Update my-script.js

--- my-script.js
+++ my-script.js
@@ -1,12 +1,8 @@
 const app = window.Telegram.WebApp;
-document.getElementById("Gayid").innerHTML = localStorage.getItem("Gay");
-
-
-//document.getElementById('Gayid').innerHTML = localStorage.getItem("Gay")
-// Call as soon as your page is ready for the user to see
 app.ready();
 function setgay(e){
     localStorage.setItem('Gay',e)
+    document.getElementById("Gayid").innerHTML = localStorage.getItem("Gay");
 }
-// Expand your web app to full screen
 app.expand();
+setgay(localStorage.getItem("Gay"));
